be846fa187dbb442812b9cedccd06eed70368e60
iaffansheikh21 <muhammadaffansheikh21@gmail.com>
2025-08-02 17:00:42 +0500

    initial commit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'APIHub',
+  description: 'APIHub is a platform for discovering and using APIs.',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
